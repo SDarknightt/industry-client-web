ee2a15b48bdc804a404e0bf0204926402c5b91c9
Samuel Dias Oliveira <samueldias004@gmail.com>
2026-02-26 17:02:03 -0300

    feat: route product definition

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,13 +1,19 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from '../pages/Home'
-import ListProduct from '../pages/products/ListProduct'
+import ProductList from '../pages/products/ProductList'
+import ProductCreate from '../pages/products/ProductCreate'
+import ProductDetails from '../pages/products/ProductDetails'
+import ProductUpdate from '../pages/products/ProductUpdate'
 
 export function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
         <Route index  element={<Home />} />
-        <Route index path="/produtos" element={<ListProduct />} />
+        <Route path="/produtos" element={<ProductList />} />
+        <Route path="/produtos/novo" element={<ProductCreate />} />
+        <Route path="/produtos/:id" element={<ProductDetails />} />
+        <Route path="/produtos/:id/editar" element={<ProductUpdate />} />
       </Routes>
     </BrowserRouter>
   )
