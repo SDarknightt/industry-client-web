d2dbc49517957700672e0a39530edcf875534a09
Samuel Dias Oliveira <samueldias004@gmail.com>
2026-02-27 14:01:57 -0300

    style: adjust back button and header style

--- src/components/layout/HeaderComponent.tsx
+++ src/components/layout/HeaderComponent.tsx
@@ -1,13 +1,10 @@
 import IndustrySvg  from '../../assets/industry-svgrepo.svg?react';
 
 export default function Header() {
-    return (
-        <header className="flex flex-row justify-between items-center px-6 bg-primary h-17.5">
-          <div>
-            Menu
-          </div>
-          <div>
-            <IndustrySvg className='h-10'/>
+  return (
+        <header className="flex flex-row items-center px-6 bg-primary h-17.5">
+          <div className="flex flex-row gap-x-2 justify-start" onClick={() => window.location.replace("/")}>
+            <span className="font-bold"><IndustrySvg className='h-10'/> Produz Mais</span>
           </div>
         </header>
     );
